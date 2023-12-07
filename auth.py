d7f498a0bf760a9e805b582c16bc61772bf7caaf
Daniel Marai <daniel.marai@continental-corporation.com>
2023-12-07 16:59:55 +0100

    Updated script for new corporate policies

--- auth.py
+++ auth.py
@@ -5,7 +5,9 @@ from urllib.parse import parse_qs
 
 
 POLICY = "B2C_1A_Parkanizer_Login"
-PARKANIZER_LOGIN_URI = "https://login.parkanizer.com/loginparkanizer.onmicrosoft.com/" + POLICY
+PARKANIZER_LOGIN_URI = (
+    "https://login.parkanizer.com/loginparkanizer.onmicrosoft.com/" + POLICY
+)
 PARKANIZER_SHARE_URI = "https://share.parkanizer.com"
 PARKANIZER_CALLBACK_URI = "https://share.parkanizer.com/callback"
 
@@ -15,60 +17,45 @@ def get_token(username, password):
 
     authorize_response = session.get(
         PARKANIZER_SHARE_URI + "/api/auth0/authorize",
-        params={
-            "callbackUri": PARKANIZER_CALLBACK_URI,
-            "emailHint": ""
-        }
+        params={"callbackUri": PARKANIZER_CALLBACK_URI, "emailHint": ""},
     )
     authorize_response.raise_for_status()
 
     state_properties = re.findall(
-        r'"transId":"(StateProperties=[^"]*)"', authorize_response.text)[0]
+        r'"transId":"(StateProperties=[^"]*)"', authorize_response.text
+    )[0]
     csrf = authorize_response.cookies["x-ms-cpim-csrf"]
 
-    auth_params = {
-        "tx": state_properties,
-        "p": POLICY
-    }
+    auth_params = {"tx": state_properties, "p": POLICY}
 
     self_asserted_response = session.post(
         PARKANIZER_LOGIN_URI + "/SelfAsserted",
         params=auth_params,
-        data={
-            "request_type": "RESPONSE",
-            "signInName": username
-        },
-        headers={
-            "X-CSRF-TOKEN": csrf
-        }
+        data={"request_type": "RESPONSE", "signInName": username},
+        headers={"X-CSRF-TOKEN": csrf},
     )
     self_asserted_response.raise_for_status()
 
     confirm_response = session.get(
         PARKANIZER_LOGIN_URI + "/api/CombinedSigninAndSignup/confirmed",
-        params=dict(auth_params, **{"csrf_token": csrf})
+        params=dict(auth_params, **{"csrf_token": csrf}),
     )
     confirm_response.raise_for_status()
 
-    csrf2 = confirm_response.cookies['x-ms-cpim-csrf']
+    csrf2 = confirm_response.cookies["x-ms-cpim-csrf"]
 
-    self_asserted2_response = session.post(
+    login_response = session.post(
         PARKANIZER_LOGIN_URI + "/SelfAsserted",
         params=auth_params,
-        data={
-            "request_type": "RESPONSE",
-            "signInName": username,
-            "password": password
-        },
-        headers={
-            "X-CSRF-TOKEN": csrf2
-        }
+        data={"request_type": "RESPONSE", "signInName": username, "password": password},
+        headers={"X-CSRF-TOKEN": csrf2},
     )
-    self_asserted2_response.raise_for_status()
+    login_response.raise_for_status()
+    assert login_response.json()["status"] == "200", login_response.json()["message"]
 
     confirm2_response = session.get(
         PARKANIZER_LOGIN_URI + "/api/CombinedSigninAndSignup/confirmed",
-        params=dict(auth_params, **{"csrf_token": csrf2})
+        params=dict(auth_params, **{"csrf_token": csrf2}),
     )
     confirm2_response.raise_for_status()
 
@@ -77,10 +64,10 @@ def get_token(username, password):
     get_token_response = session.post(
         PARKANIZER_SHARE_URI + "/api/auth0/get-token",
         json={
-            "code": confirm2_params['code'][0],
+            "code": confirm2_params["code"][0],
             "redirectUri": PARKANIZER_CALLBACK_URI,
-            "state": confirm2_params['state'][0]
-        }
+            "state": confirm2_params["state"][0],
+        },
     )
     get_token_response.raise_for_status()
 
